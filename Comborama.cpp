ccb1e38b383e269f51be0f0051b3a3a7a6c5144a
Leon Enders <m.enders.leon@gmail.com>
2024-10-10 21:46:57 +0200

    test draw

--- Comborama.cpp
+++ Comborama.cpp
@@ -22,17 +22,49 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    if (!renderer) {
+        printf("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+
+ 
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); 
+    SDL_RenderClear(renderer);
+
+   
+    SDL_FRect rect;
+    rect.x = 200;
+    rect.y = -150;
+    rect.w = 50;
+    rect.h = 50;
+
+  
+
+
     bool running = true;
     SDL_Event event;
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
+    while (running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+           
+            SDL_RenderPresent(renderer);
+
+
+            if (event.type == SDL_EVENT_QUIT)
+            {
                 running = false;
             }
+            SDL_RenderClear(renderer);
         }
     }
 
-   
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
 
