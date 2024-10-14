3740b4d47b71fbcdddf5f89d2a0e149bbdea9d6f
Leon Enders <m.enders.leon@gmail.com>
2024-10-14 10:22:42 +0200

    Controller handles move

--- Source/Controller.cpp
+++ Source/Controller.cpp
@@ -1,17 +1,32 @@
 #include "Controller.h"
 #include "Game.h"
+#include "Actor.h"
+#include "Vector2.h"
 
 
 Controller::Controller(Game* InOwningGame)
 	:
 	OwningGame(InOwningGame)
 {
-
-	QuitAction = std::make_shared<InputAction>(std::bind(&Game::QuitGame, OwningGame));
 	ActionContext = std::make_unique<InputActionContext>();
 	ControllerInputComponent = std::make_unique<InputComponent>();
 
+	QuitAction = std::make_shared<InputAction>(std::bind(&Game::QuitGame, OwningGame));
+	MoveAction = std::make_shared<InputAction>(std::bind(&Controller::Move, this));
+
+
+
 	Initialize();
+}
+
+void Controller::PossessActor(Actor* ActorToPossess)
+{
+	ControlledActor = ActorToPossess;
+}
+
+void Controller::Move(const Vector2& InVelocity)
+{
+	ControlledActor->UpdateVelocity(InVelocity);
 }
 
 void Controller::Initialize()
@@ -19,4 +34,5 @@ void Controller::Initialize()
 	ActionContext->AddInputAction(SDLK_ESCAPE, QuitAction);
 
 	ControllerInputComponent->SetInputActionContext(ActionContext.get());
+
 }
--- Source/Controller.h
+++ Source/Controller.h
@@ -3,19 +3,34 @@
 #include "Input/InputComponent.h"
 #include "Input/InputAction.h"
 
+
 //TODO: This class creates InputActions and an InputActionContext
+struct Vector2;
 class Game;
+class Actor;
+
 class Controller
 {
 public:
 	
 	Controller(Game* InOwningGame);
-	void Initialize();
+
+	void PossessActor(Actor* ActorToPossess);
 
 private:
 
+	void Move(const Vector2& InVelocity);
+
+	void Initialize();
+
+private:
 	Game* OwningGame = nullptr;
+	Actor* ControlledActor = nullptr;
+
+
 	std::unique_ptr<InputComponent> ControllerInputComponent = nullptr;
-	std::shared_ptr<InputAction> QuitAction = nullptr;
 	std::unique_ptr<InputActionContext> ActionContext = nullptr;
+
+	std::shared_ptr<InputAction> QuitAction = nullptr;
+	std::shared_ptr<InputAction> MoveAction = nullptr;
 };
