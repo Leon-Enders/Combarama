cfd1bac04234e2491ec8732989b23a4b80d101d6
Leon Enders <m.enders.leon@gmail.com>
2024-10-23 22:47:22 +0200

    fixed build warnings

--- Source/Math/Matrix.h
+++ Source/Math/Matrix.h
@@ -13,7 +13,7 @@ struct Matrix3x3 {
     {
         for (int i = 0; i < 3; ++i)
             for (int j = 0; j < 3; ++j)
-                m[i][j] = (i == j) ? 1 : 0;
+                m[i][j] = (i == j) ? 1.f : 0.f;
     }
 
     
