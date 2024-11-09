5c9e4747ece3e26c8ab56d091956947f7293d7b1
Leon Enders <m.enders.leon@gmail.com>
2024-11-09 12:09:41 +0100

    Created Delay Awaitable

---
+++ Source/Coroutine/Delay.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <coroutine>
+#include <chrono>
+
+class Delay
+{
+public:
+    explicit Delay(std::chrono::milliseconds InDuration)
+        : Duration(InDuration), StartTime(std::chrono::steady_clock::now()) {}
+
+    
+    bool await_ready() const noexcept
+    {
+        return std::chrono::steady_clock::now() >= StartTime + Duration;
+    }
+
+  
+    void await_suspend(std::coroutine_handle<> handle) const noexcept
+    {
+       
+    }
+
+   
+    void await_resume() const noexcept {}
+
+private:
+    std::chrono::milliseconds Duration;
+    std::chrono::steady_clock::time_point StartTime;
+};
