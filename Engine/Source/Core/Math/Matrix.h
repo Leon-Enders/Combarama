d0d79d145c336157453e1ceeb3fab6e72c9ef798
Leon Enders <m.enders.leon@gmail.com>
2024-12-15 14:27:09 +0100

    Fixed Matrix class

--- Engine/Source/Core/Math/Matrix.h
+++ Engine/Source/Core/Math/Matrix.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <cmath>
 #include "SDL3/SDL_rect.h"
-#include "Transform.h"
+#include "Vector3.h"
 
 
 
@@ -9,36 +9,84 @@ class TMatrix
 {
 public:
 
-
-	//Apply Matrix To Vec2
-	//Vector2 operator*(const Vector2& rhs)
-	//{
-	//	return {}
-	//}
-
-	//Create IdentityMatrix
-	static TMatrix Identity()
+	Vector2 operator*(const Vector2& rhs)const
 	{
-		return {
-			1.f,0.f,1.f,
-			0.f,1.f,1.f,
-			0.f,0.f,0.f
-		};
+		return Vector2(*this * Vector3(rhs));
+	}
+
+	Vector3 operator*(const Vector3& rhs)const
+	{
+		Vector3 OutVector;
+
+		OutVector.X = Cells[0][0] * rhs.X + Cells[0][1] * rhs.Y + Cells[0][2] + rhs.W;
+		OutVector.Y = Cells[1][0] * rhs.X + Cells[1][1] * rhs.Y + Cells[1][2] + rhs.W;
+		OutVector.W = Cells[2][0] * rhs.X + Cells[2][1] * rhs.Y + Cells[2][2] + rhs.W;
+
+		return OutVector;
+	}
+
+	TMatrix operator*(const TMatrix& rhs)const
+	{
+		TMatrix OutMatrix;
+		for (int row_left = 0; row_left < 3; row_left++)
+		{
+			for (int col_right = 0; col_right < 3; col_right++)
+			{
+				for (int i = 0; i < 3; i++)
+				{	
+					OutMatrix.Cells[row_left][col_right] += Cells[row_left][i] * rhs.Cells[i][col_right];
+				}
+			}
+		}
+		return OutMatrix;
 	}
 
 	//Create ScaleMatrix
-	static TMatrix Scale(const Vector2& Scale)
+	static TMatrix Scale(const float Scale)
 	{
 		// Use ApplyMatrix to Vec2 here, and simply Use Identity scaled by the scale
 		return {
-			1.f*Scale.X,0.f*Scale.X,1.f,
-			0.f*Scale.Y,1.f*Scale.Y,1.f,
-			0.f,0.f,0.f
+			Scale,0.f,0.f,
+			0.f,Scale,0.f,
+			0.f,0.f,1.f
 		};
 	}
 
-	//Create RotationMatrix
-	//Create TranslationMatrix
 
-	float Matrix[3][3] = {0};
+	static TMatrix Identity()
+	{
+		return Scale(1.f);
+	}
+
+	static TMatrix FlipY()
+	{
+		return {
+			1.f,0.f,0.f,
+			0.f,-1.f,0.f,
+			0.f,0.f,1.f
+		};
+	}
+
+	
+	static TMatrix Rotate(const float Theta)
+	{
+		const auto sinT = std::sin(Theta);
+		const auto cosT = std::cos(Theta);
+
+		return {
+		cosT, -sinT,0.f,
+		sinT,-cosT,0.f,
+		0.f,0.f,1.f
+		};
+	}
+	
+	static TMatrix Translate(const Vector2& Translation)
+	{
+		return{
+			1.f,0.f,Translation.X,
+			0.f,1.f,Translation.Y,
+			0.f,0.f,1.f
+		};
+	}
+	float Cells[3][3] = {0};
 };
