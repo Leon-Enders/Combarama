1edc9f1e5aa0a0c5f02306916ced3429b1cce7ae
Leon Enders <m.enders.leon@gmail.com>
2024-12-17 19:43:50 +0100

    RenderScene

---
+++ Engine/Source/Engine/GameFramework/Scene/RenderScene.cpp
@@ -0,0 +1,17 @@
+#include "RenderScene.h"
+#include <algorithm>
+#include "../Component/PrimitiveComponent.h"
+
+
+void RenderScene::RegisterPrimitiveComponent(PrimitiveComponent* PrimitiveComponentToRegister)
+{
+	PrimitiveComponents.push_back(PrimitiveComponentToRegister);
+}
+
+void RenderScene::UnRegisterPrimitiveComponent(PrimitiveComponent* PrimitiveComponentToRegister)
+{
+	std::erase_if(PrimitiveComponents, [&](PrimitiveComponent* PrimitiveComp)
+		{
+			return PrimitiveComp == PrimitiveComponentToRegister;
+		});
+}
--- Engine/Source/Engine/GameFramework/Scene/RenderScene.h
+++ Engine/Source/Engine/GameFramework/Scene/RenderScene.h
@@ -1,1 +1,14 @@
 #pragma once
+#include <vector>
+
+class PrimitiveComponent;
+
+class RenderScene
+{
+public:
+	void RegisterPrimitiveComponent(PrimitiveComponent* PrimitiveComponentToRegister);
+	void UnRegisterPrimitiveComponent(PrimitiveComponent* PrimitiveComponentToRegister);
+
+private:
+	std::vector<PrimitiveComponent*> PrimitiveComponents;
+};
