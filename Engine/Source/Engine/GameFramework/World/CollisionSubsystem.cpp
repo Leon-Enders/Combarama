ad08fc5ef7a4d46cb5638f40df6ac1109ba18014
Leon Enders <m.enders.leon@gmail.com>
2024-12-18 11:59:54 +0100

    Added TODO

--- Engine/Source/Engine/GameFramework/World/CollisionSubsystem.cpp
+++ Engine/Source/Engine/GameFramework/World/CollisionSubsystem.cpp
@@ -22,7 +22,11 @@ bool CollisionSubsystem::SweepByChannel(const PhysicsScene& PScene,
 	ECollisionChannel CollisionChannel, 
 	Actor* ActorToIgnore)const
 {
-
+	//TODO: Handle CollisionQuery
+	// Iterate through the scene and check if the distance of the sweep is less than the distance to a bodyproxy
+	// this results in a list of valid targets
+	// sweep the shape against each valid target
+	// Read how about how you would sweep?
 
 
 	return false;
