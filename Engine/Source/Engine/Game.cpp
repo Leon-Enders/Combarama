338d64b46e30d5678d3cd45d7c4ecfaa0709922d
Leon Enders <m.enders.leon@gmail.com>
2024-12-25 15:01:09 +0100

    new level

--- Engine/Source/Engine/Game.cpp
+++ Engine/Source/Engine/Game.cpp
@@ -75,6 +75,10 @@ void Game::StartGame()
 	Transform ObstacleTransform5;
 	Transform ObstacleTransform6;
 	Transform ObstacleTransform7;
+	Transform ObstacleTransform8;
+	Transform ObstacleTransform9;
+	Transform ObstacleTransform10;
+	Transform ObstacleTransform11;
 
 
 
@@ -85,8 +89,20 @@ void Game::StartGame()
 	ObstacleTransform3.Position = { 0.f,1000.f};
 	ObstacleTransform4.Position = { 0.f,-1000.f};
 	ObstacleTransform5.Position = { 0.f,0.f };
+	ObstacleTransform5.Rotation = 0.7f;
 	ObstacleTransform6.Position = { -200.f,50.f};
+	ObstacleTransform6.Rotation = -0.3f;
 	ObstacleTransform7.Position = { 400.f,-300.f };
+	ObstacleTransform7.Rotation = 0.45f;
+	ObstacleTransform8.Position = { 700.f,200.f };
+	ObstacleTransform8.Rotation = -0.6f;
+	ObstacleTransform9.Position = { -400.f,350.f };
+	ObstacleTransform9.Rotation = 1.4f;
+	ObstacleTransform10.Position = { -500.f, -300.f };
+	ObstacleTransform10.Rotation = -0.5f;
+	ObstacleTransform11.Position = { -500.f, 600.f };
+	ObstacleTransform11.Rotation = 1.f;
+
 
 	GameWorld->SpawnGameObject<Obstacle>(ObstacleTransform1, Vector2(50.f, 1000.f), COLOR_PURPLE);
 	GameWorld->SpawnGameObject<Obstacle>(ObstacleTransform2, Vector2(50.f, 1000.f), COLOR_PURPLE);
@@ -95,6 +111,12 @@ void Game::StartGame()
 	GameWorld->SpawnGameObject<Obstacle>(ObstacleTransform5, Vector2(25.f, 50.f), COLOR_PURPLE);
 	GameWorld->SpawnGameObject<Obstacle>(ObstacleTransform6, Vector2(50.f, 100.f), COLOR_PURPLE);
 	GameWorld->SpawnGameObject<Obstacle>(ObstacleTransform7, Vector2(300.f, 50.f), COLOR_PURPLE);
+	GameWorld->SpawnGameObject<Obstacle>(ObstacleTransform8, Vector2(100.f, 50.f), COLOR_PURPLE);
+	GameWorld->SpawnGameObject<Obstacle>(ObstacleTransform9, Vector2(100.f, 50.f), COLOR_PURPLE);
+	GameWorld->SpawnGameObject<Obstacle>(ObstacleTransform10, Vector2(300.f, 50.f), COLOR_PURPLE);
+	GameWorld->SpawnGameObject<Obstacle>(ObstacleTransform11, Vector2(300.f, 50.f), COLOR_PURPLE);
+
+	
 	StartGameLoop();
 }
 
